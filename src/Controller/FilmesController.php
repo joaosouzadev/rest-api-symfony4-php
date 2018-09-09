425328c372952dc4f3cddb186373649e040153f1
João Victor Souza <jvms3d@gmail.com>
2018-09-09 19:44:11 -0300

    Método POST

--- src/Controller/FilmesController.php
+++ src/Controller/FilmesController.php
@@ -5,16 +5,34 @@ namespace App\Controller;
 use App\Entity\Filme;
 use FOS\RestBundle\Controller\ControllerTrait;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FilmesController extends AbstractController {
 
 	use ControllerTrait;
 
+	/**
+	* @Rest\View()
+	*/
 	public function getFilmesAction() {
 
 		$filmes = $this->getDoctrine()->getRepository(Filme::class)->findAll();
 
 		return $filmes;
+	}
+
+	/**
+	* @Rest\View(statusCode=201)
+	* @ParamConverter("filme", converter="fos_rest.request_body")
+	* @Rest\NoRoute()
+	*/
+	public function postFilmesAction(Filme $filme) {
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($filme);
+		$em->flush();
+
+		return $filme;
 	}
 }
